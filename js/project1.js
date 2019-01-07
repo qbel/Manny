6b2f43fb1eae3d2dbc2bb5673caac9c1ffb3003d
qbel <queenjummy0@gmail.com>
2019-01-07 02:14:06 +0100

    first commit

---
+++ js/project1.js
@@ -0,0 +1,5 @@
+// SideNav Button Initialization
+$(".button-collapse").sideNav();
+// SideNav Scrollbar Initialization
+var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+Ps.initialize(sideNavScrollbar);
